8ecacea6ccb8fa921f22b4c0c41d26f14dd280cd
mobashirsifat123 <2023521460117@stu.scu.edu.cn>
2026-04-10 14:38:47 +0800

    fix: Next 15 linting path resolution in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
-import nextTypescript from "eslint-config-next/typescript";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript.js";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals.js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
