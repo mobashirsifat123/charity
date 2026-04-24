ba3789110716912db58016f2b41a9f5f5715fff8
mobashirsifat123 <2023521460117@stu.scu.edu.cn>
2026-04-24 15:15:34 +0800

    Disable unstable Bengali DOM translator

--- src/components/ClientProviders.jsx
+++ src/components/ClientProviders.jsx
@@ -1,26 +1,24 @@
 "use client";
 import { AuthProvider } from "@/context/AuthContext";
 import { AudioPlayerProvider } from "@/context/AudioPlayerContext";
-import BengaliAutoTranslator from "@/components/BengaliAutoTranslator";
 import { LanguageProvider } from "@/context/LanguageContext";
 import { ReadabilityProvider } from "@/context/ReadabilityContext";
 import { SiteSettingsProvider } from "@/context/SiteSettingsContext";
 import InitializeAOS from "@/helper/InitializeAOS";
 
 export default function ClientProviders({ children }) {
-    return (
-        <LanguageProvider>
-            <AudioPlayerProvider>
-                <ReadabilityProvider>
-                    <SiteSettingsProvider>
-                        <AuthProvider>
-                            <InitializeAOS />
-                            <BengaliAutoTranslator />
-                            {children}
-                        </AuthProvider>
-                    </SiteSettingsProvider>
-                </ReadabilityProvider>
-            </AudioPlayerProvider>
-        </LanguageProvider>
-    );
+  return (
+    <LanguageProvider>
+      <AudioPlayerProvider>
+        <ReadabilityProvider>
+          <SiteSettingsProvider>
+            <AuthProvider>
+              <InitializeAOS />
+              {children}
+            </AuthProvider>
+          </SiteSettingsProvider>
+        </ReadabilityProvider>
+      </AudioPlayerProvider>
+    </LanguageProvider>
+  );
 }
